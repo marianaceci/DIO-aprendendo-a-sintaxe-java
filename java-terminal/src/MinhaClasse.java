23cc72f7f3794d2af314f9f491993a460f2d10d0
Mariana Schmidt <marianaceciliaschmidt@gmail.com>
2023-02-22 13:48:44 -0300

    executar classe no cmd

---
+++ java-terminal/src/MinhaClasse.java
@@ -0,0 +1,8 @@
+public class MinhaClasse {
+  public static void main(String[] args) {
+    System.out.println("Oiiii, turma, fui executado pelo Terminal");
+  }
+  
+}
+
+// para executar na linha de comando, deve-se ir a pasta bin e 'java MinhaClasse' após compilação do arquivo pela IDE
