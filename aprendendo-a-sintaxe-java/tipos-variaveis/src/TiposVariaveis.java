dc12371fa2e838bd47675abbac2edcfca7093a75
Mariana Schmidt <marianaceciliaschmidt@gmail.com>
2023-02-22 11:35:57 -0300

    tipos de variaveis

--- aprendendo-a-sintaxe-java/tipos-variaveis/src/TiposVariaveis.java
+++ aprendendo-a-sintaxe-java/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        //VAR vs. CONST
+        int numero = 5;
+        numero = 10;
+        System.out.print(numero); //10
+
+        final double VALOR_DE_PI = 3.14; //final não pode ser alterado		
+        //VALOR_DE_PI=3.15; //Esta linha vai apresentar erro de compilação!
+    
+        //ver tipos primitivos
     }
 }
